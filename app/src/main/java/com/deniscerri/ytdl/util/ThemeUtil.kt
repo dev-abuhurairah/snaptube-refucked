ec4b40af80e5bbe463acf9ad27c60828ac764ad6
dev-abuhurairah <contactabuhurairah@gmail.com>
2026-09-07 08:53:42 +0500

    fix(theme): import android.graphics.Color in ThemeUtil.kt

--- app/src/main/java/com/deniscerri/ytdl/util/ThemeUtil.kt
+++ app/src/main/java/com/deniscerri/ytdl/util/ThemeUtil.kt
@@ -5,6 +5,7 @@ import android.app.Application
 import android.content.ComponentName
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Spanned
 import android.util.TypedValue
